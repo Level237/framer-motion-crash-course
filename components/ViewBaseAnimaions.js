94a0b628387768bdfbe6886fe5151aff96f7c8d1
level237 <Bramslevel129@gmail.com>
2024-02-19 07:49:09 +0100

    framer-motion:view base animations

--- components/ViewBaseAnimaions.js
+++ components/ViewBaseAnimaions.js
@@ -1,22 +1,40 @@
 import React, { useEffect, useRef } from "react";
-
+import {motion,useInView} from "framer-motion"
 
 const ViewBasedAnimations = () => {
 
+  const ref=useRef(null)
+  const isInView=useInView(ref,{
+    once:true
+  })
 
-
-
+  useEffect(()=>{
+    console.log("is in view:",isInView);
+  },[isInView])
   return (
     <>
       <div style={{ height: "150vh" }} />
     
-      <div
-        ref={ref}
+      <motion.div
         style={{
           height: "100vh",
-          background: isInView ? "blue" : "red",
-          transition: "1s background",
+          background:"black"
         }}
+        initial={{ 
+          opacity:0
+         }}
+         whileInView={{ 
+          opacity:1
+          }}
+          transition={{ duration:1 }}
+      />
+      <div
+      ref={ref}
+      style={{ 
+        height:'100vh',
+        background:isInView ? "blue":'red',
+        transition:"2s background"
+       }}
       />
     </>
   );
